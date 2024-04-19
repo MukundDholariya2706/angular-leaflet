c982ef98c33a851a502e81650f74e0fd1eabaa92
Mukund Dholariya <mukunddtridhyatech@gmail.com>
2024-04-19 13:29:27 +0530

    vercel backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // API_URL: "https://node-leaflet.vercel.app",
-  API_URL: "https://nodejs-production-08210.up.railway.app",
+  API_URL: "https://node-leaflet.vercel.app",
+  // API_URL: "https://nodejs-production-08210.up.railway.app",
   PUSHER_KEY: "5ad67e7b101ae5244ad7",
   PUSHER_CLUSTER: 'ap2'
 };
